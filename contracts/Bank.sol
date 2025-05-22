5b78875edbbfdb674100a224fad7eb5bbde1d9d7
ThibautBaudry <thibaut.baudry@gmail.com>
2025-05-22 15:23:26 +0200

    Deposit function

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -6,6 +6,10 @@ contract Bank {
 
     mapping(address => uint) _balances;
 
+    function deposit (uint _amount) public {
+        _balances[msg.sender] += _amount;
+    }
+
     function getBalance (address _address) public view returns (uint) {
         return _balances[_address];
     }
