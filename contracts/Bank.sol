adf16801b3ca0877e7448f313baa65e3c2b062d0
ThibautBaudry <thibaut.baudry@gmail.com>
2025-05-22 16:18:12 +0200

    NatSpec

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -2,14 +2,22 @@
 
 pragma solidity 0.8.30;
 
+/// @title Decentralized Bank
+/// @author Thibaut Baudry
+/// @notice Contrat basique représantant une banque Web3
+
 contract Bank {
 
     mapping(address => uint) _balances;
 
+    /// @notice Dépot d'un montant sur un compte
+    /// @param _amount Le montant
     function deposit (uint _amount) public {
         _balances[msg.sender] += _amount;
     }
 
+    /// @notice Transfère d'un montant d'un compte à un autre
+    /// @param _amount Le montant
     function transfer (address _to, uint _amount) public {
         require(_to != address (0), "Address 0");
         require(_balances[msg.sender] >= _amount, "Not enough funds");
@@ -17,6 +25,8 @@ contract Bank {
         _balances[msg.sender] -= _amount;
     }
 
+    /// @notice Récupère la balance de l'addresse entrée en paramètre
+    /// @return uint La balance de l'addresse 
     function getBalance (address _address) public view returns (uint) {
         return _balances[_address];
     }
